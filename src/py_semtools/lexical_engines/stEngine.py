76a99d2388b1a50c35d32b56b52baec5c77ec460
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2026-03-02 13:20:24 +0100

    Added an else condition raising an Exception if no gpu devices were provided for 'make_rerank'

--- src/py_semtools/lexical_engines/stEngine.py
+++ src/py_semtools/lexical_engines/stEngine.py
@@ -198,7 +198,6 @@ class STengine(LexicalEngineBaseClass):
             results[query_idx].append({'score': score, 'corpus_id': corpus_idx})
         return results
 
-
     def make_rerank(self, sentence_pairs, options):
         import torch
         if options['verbose']: print(f"---Reranking {len(sentence_pairs)} sentence pairs with cross-encoder model")
@@ -208,7 +207,9 @@ class STengine(LexicalEngineBaseClass):
                 scores = self.reranker.predict(sentence_pairs, pool=pool, batch_size=options["batch_size"], chunk_size=options['single_worker_chunk_size'], activation_fn=torch.nn.Sigmoid())
                 self.reranker.stop_multi_process_pool(pool)
         elif len(options["gpu_device"]) == 1:
-                scores = self.reranker.encode(sentence_pairs, device= options["gpu_device"][0], batch_size=options["batch_size"], activation_fn=torch.nn.Sigmoid()) #convert_to_tensor=True 
+                scores = self.reranker.predict(sentence_pairs, device= options["gpu_device"][0], batch_size=options["batch_size"], activation_fn=torch.nn.Sigmoid()) #convert_to_tensor=True 
+        else:
+                raise Exception("You need to provide GPU to use reranker. It should be a list of GPU devices like: ['cuda:0'] or ['cuda:0', 'cuda:1']")                
         if options["verbose"]: print(f"---Reranking time with {0 if options.get('gpu_device') == None else len(options['gpu_device'])} GPUs: {time.time() - start} seconds")
         return scores
 
